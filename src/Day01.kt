8a5cbb62a066690dd47a8f240a9f8f5ae65348f8
lucas-larsson <lucas.a.larsson@gmail.com>
2023-06-20 00:54:34 +0200

    Added a solution to Day01.kt

--- src/Day01.kt
+++ src/Day01.kt
@@ -3,15 +3,28 @@ fun main() {
         return input.size
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    val input = readInput("input/1")
+    println("Size: ${part1(input)}")
+
+    fun day01 (): IntArray {
+        val summedArray = ArrayList<Int>()
+        var sum = 0
+        for (i in input){
+            if (i == ""){
+                summedArray.add(sum)
+                sum = 0
+                continue
+            }else{
+                sum += i.toInt()
+            }
+        }
+        summedArray.sort()
+        val topValue = summedArray[summedArray.size -1]
+        val topTreeSum = topValue + summedArray[summedArray.size -2] + summedArray[summedArray.size -3]
+        return intArrayOf(topValue ,topTreeSum)
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    println("Day 1 part 1: ${day01()[0]}")
+    println("Day 1 part 2: ${day01()[1]}")
 
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
 }
